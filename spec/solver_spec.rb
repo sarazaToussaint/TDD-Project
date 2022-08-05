0459797f1d7fd0f8f8e5b876e4388ea6f58e1bc2
sarazaToussaint <tmsaraza@gmail.com>
2022-08-05 15:28:57 +0200

    Add test for the factorial faction

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,5 +5,23 @@ describe 'Write unit tests for class Solver' do
       @solver = Solver.new
     end
 
+    context '#factorial method' do
+      it 'should take one argument N and returns the factorial for that number' do
+        result = @solver.factorial(0)
+          expect(result).to eql 1
+      end
+
+      it 'should return 120 when N is equal to 6' do
+        result = @solver.factorial(5)
+        expect(result).to eql 120
+      end
+  
+      it 'should raise an exception when N is a negative integer' do
+        result = @solver.factorial(-2)
+        expect(result).to match('No negative integer. This is an exception.')
+      end
+    end
+
+    
+
 end   
- 
